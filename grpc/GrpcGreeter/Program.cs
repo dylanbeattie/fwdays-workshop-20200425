e75a3b69285600e05b896c32a3ca53f5f7b3c8c5
Dylan Beattie <dylan@dylanbeattie.net>
2020-04-25 16:55:23 +0100

    Working with HTTPS certificate

--- grpc/GrpcGreeter/Program.cs
+++ grpc/GrpcGreeter/Program.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Server.Kestrel.Core;
@@ -20,9 +21,19 @@ namespace GrpcGreeter {
 				.ConfigureWebHostDefaults(webBuilder => {
 					webBuilder.ConfigureKestrel(options => {
 						// Setup a HTTP/2 endpoint without TLS.
-						options.ListenLocalhost(5000, o => o.Protocols = HttpProtocols.Http2);
+						options.ListenAnyIP(5000, o => o.Protocols = HttpProtocols.Http2);
+						options.UseSslIfFileExists(5001, @"D:\workshop.ursatile.com.pfx");
 					});
 					webBuilder.UseStartup<Startup>();
 				});
 	}
+
+	public static class KestrelServerExtensions {
+		public static void UseSslIfFileExists(this KestrelServerOptions options, int port, string pfxFilePath, string password = null) {
+			if (File.Exists(pfxFilePath)) {
+				options.Listen(IPAddress.Any, port, listen => listen.UseHttps(pfxFilePath, password));							
+			}
+		}
+	}
 }
+
