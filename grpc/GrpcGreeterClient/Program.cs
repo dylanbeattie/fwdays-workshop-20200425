a8e90a55b6e440985a313b9caa16d4e154d38b13
Dylan Beattie <dylan@dylanbeattie.net>
2020-04-25 15:55:31 +0100

    Configure client via command line

--- grpc/GrpcGreeterClient/Program.cs
+++ grpc/GrpcGreeterClient/Program.cs
@@ -8,16 +8,26 @@ namespace GrpcGreeterClient {
 	class Program {
 		private static Stopwatch sw = new Stopwatch();
 		static async Task Main(string[] args) {
-			using (var channel = GrpcChannel.ForAddress("https://localhost:5001")) {
+			string host;
+			try {
+				host = new Uri(args[0]).ToString();
+			} catch (Exception) {
+				host = "https://localhost:5001";
+			}
+			Console.WriteLine($"Connecting to gRPC provider on {host}");
+			using (var channel = GrpcChannel.ForAddress(host)) {
 				var client = new Greeter.GreeterClient(channel);
+				var request = new HelloRequest { Name = "Test" };
+				var reply = await client.SayHelloAsync(request);
+				Console.WriteLine(reply);
 				while (true) {
 					sw.Reset();
 					Console.WriteLine(String.Empty.PadLeft(72, '='));
 					Console.Write("Enter your name: ");
 					var name = Console.ReadLine();
 					sw.Start();
-					var request = new HelloRequest { Name = name };
-					var reply = await client.SayHelloAsync(request);
+					request = new HelloRequest { Name = name };
+					reply = await client.SayHelloAsync(request);
 					sw.Stop();
 					Console.WriteLine(reply);
 					Console.WriteLine($"gRPC call took {sw.ElapsedMilliseconds} ms");
